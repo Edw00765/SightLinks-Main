43664a331e98ad8b936056d0fb23316366dba0f8
YiliuLi <peterlyl2006@outlook.com>
2025-01-16 13:30:48 +0000

    Update

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def create_dir(run_dir):
         i += 1
     output_dir = os.path.join(run_dir, timestamp)
     os.makedirs(output_dir, exist_ok=True)
+    print(f"Output directory created: {output_dir}")
     return output_dir
 
 def execute(uploadDir = "input", inputType = "0", classificationThreshold = 0.35, predictionThreshold = 0.5, saveLabeledImage = False, outputType = "0", yoloModelType = "n"):
@@ -29,5 +30,3 @@ def execute(uploadDir = "input", inputType = "0", classificationThreshold = 0.35
     prediction(predictionThreshold, saveLabeledImage, outputType, outputFolder, yoloModelType)
     print(f"Output saved to {outputFolder} as {outputType}.")
     print(f"Total time taken: {time.time() - start_time:.2f} seconds")
-
-execute(inputType = "1")
