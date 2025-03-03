8388bb4b14f76315cdadcb2d1e4b2dea80aa1e37
Edw00765 <edw00765@gmail.com>
2025-03-03 22:34:36 +0000

    Added comments for main functions used in the program.

--- georeference/georeference.py
+++ georeference/georeference.py
@@ -2,6 +2,23 @@ from pyproj import Transformer
 from osgeo import gdal, osr
 
 def georefereceJGW(x1,y1,x2,y2,x3,y3,x4,y4,pixelSizeX,pixelSizeY,topLeftXGeo,topLeftYGeo):
+    """
+    This function will receive all of the x y points from a bounding box, their respective pixel sizes, and the
+    georeferencing data of the image's top left corner.
+
+    Using this information, it will calculate the coordinates of the four corners of the bounding box.
+
+    Args:
+        x(i) (int): The x pixel location of the ith bounding box corner in the image.
+        y(i) (int): The y pixel location of the ith bounding box corner in the image.
+        pixelSizeX (int): The real-world width of each pixel
+        pixelSizeY (int): The real-world height of each pixel
+        topLeftXGeo (int): The real-world x-coordinate of the top left pixel in the image (In our case it is easting and northings)
+        topLeftYGeo (int): The real-world y-coordinate of the top left pixel in the image (In our case it is easting and northings)
+    
+    Returns:
+        list: It is a list of coordinates for each corner of the box.
+    """
     x1 = topLeftXGeo + pixelSizeX * x1
     y1 = topLeftYGeo + pixelSizeY * y1
     x2 = topLeftXGeo + pixelSizeX * x2
@@ -14,6 +31,16 @@ def georefereceJGW(x1,y1,x2,y2,x3,y3,x4,y4,pixelSizeX,pixelSizeY,topLeftXGeo,top
 
 
 def BNGtoLatLong(listOfPoints):
+    """
+    This function takes a list of coordinates, representing a corner of a bounding box in BNG. It will then convert it
+    to latitude and longitude.
+
+    Args:
+        listOfPoints (list): It is a list of coordinates for each corner of the box in BNG.
+    
+    Returns:
+        latLongList (list): It is a list of latitude and longitudes, representing the corners of a bounding box.
+    """
     transformer = Transformer.from_crs("EPSG:27700", "EPSG:4326", always_xy=True)
     latLongList = []
     for xBNG, yBNG in listOfPoints:
@@ -24,6 +51,18 @@ def BNGtoLatLong(listOfPoints):
 
 
 def georeferenceTIF(croppedTifImage, x1,y1,x2,y2,x3,y3,x4,y4):
+    """
+    This function takes a croppedTifImage, and the four corners of a bounding box. It will then use the data stored in the
+    TIF image to find the coordinates of the corners, and it is then converted to latitude and longitude.
+
+    Args:
+        croppedTifImage (tif object): It is a tif image which was already cropped and stored in memory.
+        x(i) (int): The x pixel location of the ith bounding box corner in the image.
+        y(i) (int): The y pixel location of the ith bounding box corner in the image.
+    
+    Returns:
+        outputList (list): It is a list of coordinates for each corner of the box.
+    """
     geotransform = croppedTifImage.GetGeoTransform()
     if not geotransform:
         raise ValueError(f"No geotransform found in the file: {croppedTifImage}")
--- orientedBoundingBox/predictOBB.py
+++ orientedBoundingBox/predictOBB.py
@@ -97,6 +97,23 @@ from utils.filterOutput import removeDuplicateBoxesRC, combineChunksToBaseName
 
 # These versions have filtering built in
 def predictionJGW(imageAndDatas, predictionThreshold=0.25, saveLabeledImage=False, outputFolder="run/output", modelType="n"):
+    """
+    This function will take all of the segmented image and their georeferencing data from imageAndDatas, where the model then 
+    processes the image and  creates a list of bounding boxes. It then takes each bounding box, georeferences it, and then 
+    stores it in the dictionary imageDetectionsRowCol, where the key stores the basename, row, and column which this bounding 
+    box came from. After looping through all of the items, it is then filtered to reduce duplications. This filter also 
+    removes the row and column data, storing all of the bounding boxes from one image with the image name as the key.
+
+    Args:
+        imageAndDatas (list): A list of the input image name, segmented image, georeferencing data, row, and column.
+        predictionThreshold (float): The confidence threshold for the bounding box model.
+        saveLabeledImage (bool): If true, the images with bounding boxes will be saved.
+        outputFolder (str): This directs where the model should save the output to.
+        modelType (str): The type of model used.
+    
+    Returns:
+        imageDetections (dict): A dictionary where the basename of an image is the key, and the key stores a list of boxes in latitude and longitude, and their respective confidence
+    """
     modelPath = f"models/yolo-{modelType}.pt"
     model = YOLO(modelPath)  # load an official model
     # Dictionary to store all detections and their confidence grouped by original image
@@ -118,18 +135,19 @@ def predictionJGW(imageAndDatas, predictionThreshold=0.25, saveLabeledImage=Fals
                     for confidence in result.obb.conf:
                         allConfidenceList.append(confidence.item())
                     for boxes in result.obb.xyxyxyxy:
-                        x1, y1 = boxes[0]
-                        x2, y2 = boxes[1]
-                        x3, y3 = boxes[2]
-                        x4, y4 = boxes[3]
+                        x1, y1 = boxes[0].tolist()
+                        x2, y2 = boxes[1].tolist()
+                        x3, y3 = boxes[2].tolist()
+                        x4, y4 = boxes[3].tolist()
                         listOfPoints = georefereceJGW(x1,y1,x2,y2,x3,y3,x4,y4,pixelSizeX,pixelSizeY,topLeftXGeo,topLeftYGeo)
                         latLongList = BNGtoLatLong(listOfPoints)
                         allPointsList.append(latLongList)
-                    if allPointsList:
-                        baseNameWithRowCol = f"{baseName}__r{row}__c{col}"
-                        imageDetectionsRowCol[baseNameWithRowCol] = [allPointsList,allConfidenceList]
+                if allPointsList:
+                    baseNameWithRowCol = f"{baseName}__r{row}__c{col}"
+                    imageDetectionsRowCol[baseNameWithRowCol] = [allPointsList,allConfidenceList]
             except Exception as e:
                 print(f"Error processing {croppedImage}: {e}")
+                print(traceback.format_exc())
             pbar.update(1)
         
     removeDuplicateBoxesRC(imageDetectionsRowCol=imageDetectionsRowCol)
@@ -138,6 +156,23 @@ def predictionJGW(imageAndDatas, predictionThreshold=0.25, saveLabeledImage=Fals
 
 # This version of predictionTIF has filtering
 def predictionTIF(imageAndDatas, predictionThreshold=0.25, saveLabeledImage=False, outputFolder="run/output", modelType="n"):
+    """
+    This function will take all of the segmented image and their georeferencing data from imageAndDatas, where the model then 
+    processes the image and  creates a list of bounding boxes. It then takes each bounding box, georeferences it, and then 
+    stores it in the dictionary imageDetectionsRowCol, where the key stores the basename, row, and column which this bounding 
+    box came from. After looping through all of the items, it is then filtered to reduce duplications. This filter also 
+    removes the row and column data, storing all of the bounding boxes from one image with the image name as the key.
+
+    Args:
+        imageAndDatas (list): A list of the input image name, segmented tif image, row, and column.
+        predictionThreshold (float): The confidence threshold for the bounding box model.
+        saveLabeledImage (bool): If true, the images with bounding boxes will be saved.
+        outputFolder (str): This directs where the model should save the output to.
+        modelType (str): The type of model used.
+    
+    Returns:
+        imageDetections (dict): A dictionary where the basename of an image is the key, and the key stores a list of boxes in latitude and longitude, and their respective confidence.
+    """
     modelPath = f"models/yolo-{modelType}.pt"
     model = YOLO(modelPath)  # load an official model
     # Dictionary to store all detections and their confidence grouped by image, row, and column
@@ -159,22 +194,17 @@ def predictionTIF(imageAndDatas, predictionThreshold=0.25, saveLabeledImage=Fals
                 if saveLabeledImage and os.path.exists(outputFolder+"/labeledImages/run/image0.jpg"):
                     os.rename(outputFolder+"/labeledImages/run/image0.jpg", outputFolder+f"/labeledImages/run/image{numOfSavedImages}.jpg")
                     numOfSavedImages += 1
-                print(results)
                 for result in results:
-                    print(result)
                     result = result.cpu()
                     for confidence in result.obb.conf:
-                        print("confidence", confidence)
                         allConfidenceList.append(confidence.item())
                     for boxes in result.obb.xyxyxyxy:
-                        print("boxes:", boxes)
                         x1, y1 = boxes[0].tolist()
                         x2, y2 = boxes[1].tolist()
                         x3, y3 = boxes[2].tolist()
                         x4, y4 = boxes[3].tolist()
                         longLatList = georeferenceTIF(croppedImage,x1,y1,x2,y2,x3,y3,x4,y4)
                         allPointsList.append(longLatList)
-                
                 if allPointsList:
                     baseNameWithRowCol = f"{baseName}__r{row}__c{col}"
                     imageDetectionsRowCol[baseNameWithRowCol] = [allPointsList,allConfidenceList]
